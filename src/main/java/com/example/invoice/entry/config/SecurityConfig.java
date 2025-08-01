b195a874db336bd11c223de5d281a17daedbe904
Yim.H <heim@businessinsight.co.kr>
2025-08-01 15:56:43 +0900

    레이아웃 헤더

--- src/main/java/com/example/invoice/entry/config/SecurityConfig.java
+++ src/main/java/com/example/invoice/entry/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 				.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/login", "/signup", "/static/css/**").permitAll()
+						.requestMatchers("/login", "/signup", "/css/**").permitAll()
 						.anyRequest().authenticated()
 				)
 				.formLogin(form -> form
